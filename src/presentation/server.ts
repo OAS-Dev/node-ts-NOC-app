7bb038223ed5d50d64559d1afa49005ad4b89dd7
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2024-08-28 17:30:30 -0300

    🎉 NOC
    NOC App - NodeJS Example

---
+++ src/presentation/server.ts
@@ -0,0 +1,16 @@
+import {CheckService} from '../domain/use-cases/checks/check-service';
+import {CronService} from './cron/cron-service';
+
+export class Server {
+  public static start() {
+    console.log('Server started');
+
+    CronService.createJob('*/5 * * * * *', () => {
+      new CheckService(
+        () => console.log('Service is OK'),
+        (error) => console.log(`Error on check service: ${error}`),
+      ).execute('https://google.com');
+      //   new CheckService().execute('http://localhost:3000');
+    });
+  }
+}
